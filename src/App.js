09de79ef98d998005b6ef573ec05c80d22145bce
SungJoonKim <88412911+ksj0818@users.noreply.github.com>
2021-11-17 17:54:44 +0900

    state에 mode값 넣어주기 및 모드값에 따라 본문 출력 문자 변경 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -2,25 +2,63 @@ import { Component } from "react";
 import "./App.css";
 import TOC from "./components/TOC";
 import Content from "./components/Content";
-import Subject from "./components/Subject";
+// import Subject from "./components/Subject";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    // state값 초기화
     this.state = {
-      subject: {}
-    }
+      mode: "read",
+      welcome: { title: "Welcome", desc: "Hello, React!!!" },
+      subject: { title: "WEB!", sub: "World Wide Web" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is HyperText..." },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JS", desc: " JS is for interactive" },
+      ],
+    };
   }
   render() {
+    let _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
     return (
       <div>
-        <Subject title="Web" sub="world wide web"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content
-          title="HTML"
-          sub="HTML is HyperText Markup Language."
-        ></Content>
+        {/* <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject> */}
+        <header>
+          <h1>
+            <a
+              href="/"
+              onClick={function (e) {
+                e.preventDefault();
+                // 이벤트가 호출되었을때 실행되는 함수 안에서는 this의 값이 컴포넌트 자기 자신을 가리키지 않고 아무 값도 세팅 되어있지 않음. 그렇기 때문에 undefined의 state를 읽으려고 했더니 error발생.
+                // 이벤트를 설치할 때 this를 찾을 수 없어서 error가 발생하면 그냥 .bind(this)함수를 이벤트함수가 끝난 직후에 넣어주면된다.
+                // this.state.mode = "welcome"; 이렇게 짜면 리액트는 state가 바뀐걸 모름
+                this.setState({
+                  mode: "welcome",
+                });
+              }.bind(this)}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          <h4>{this.state.subject.sub}</h4>
+        </header>
+
+        {/*데이터가 바뀌었다고 TOC의 로직을 바꿀 필요가 없다. */}
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} sub={_desc}></Content>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,19 +2,26 @@ import React, { Component } from "react";
 
 class TOC extends Component {
   render() {
+    let data = this.props.data;
+    let i = 0;
+    let list = [];
+    // 반복문이 실행될 때마다 list배열에 push하기.
+    while (i < data.length) {
+      list.push(
+        /* 
+          엘리먼트 여러 개를 자동으로 생성 시 콘솔창에 error 발생한다.
+          Why? 각각의 리스트 항목들은 key라고 하는 props를 가지고 있어야 하기 때문이다.
+        */
+        <li key={data[i].id}>
+          {/* 3. list 항목 링크 클릭 시 해당컨텐츠가 본문 내용에 출력 */}
+          <a href={"/content/" + data[i].id}>{data[i].title}</a>
+        </li>
+      );
+      i += 1;
+    }
     return (
       <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JS</a>
-          </li>
-        </ul>
+        <ul>{list}</ul>
       </nav>
     );
   }
