71e796a3fc534b993eb0cc42f64ec5d017649423
SungJoonKim <88412911+ksj0818@users.noreply.github.com>
2021-11-18 17:51:53 +0900

    sj브랜치 스타트

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -3,20 +3,10 @@ import React, { Component } from "react";
 class Subject extends Component {
   render() {
     return (
-      <header>
-        <h1>
-          <a
-            href="/"
-            onClick={function (e) {
-              e.preventDefault();
-              this.props.onChangePage(); // App.js에서 전달한 함수를 props로 받아서 호출
-            }.bind(this)}
-          >
-            {this.props.title}
-          </a>
-        </h1>
-        {this.props.sub}
-      </header>
+      <div>
+        <h1>{this.props.title}</h1>
+        <h3>{this.props.sub}</h3>
+      </div>
     );
   }
 }
