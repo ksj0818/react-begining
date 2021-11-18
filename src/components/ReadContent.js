2a9267986950176d594921da36d7275d84c1af8a
SungJoonKim <88412911+ksj0818@users.noreply.github.com>
2021-11-18 17:34:19 +0900

    읽기 컴포넌트 생성

---
+++ src/components/ReadContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ReadContent extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        <h3>{this.props.desc}</h3>
+      </article>
+    );
+  }
+}
+
+export default ReadContent;
