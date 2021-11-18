e33d11b7ef7e0a1acab90addf023ecc215e262d9
SungJoonKim <88412911+ksj0818@users.noreply.github.com>
2021-11-18 18:00:51 +0900

    컨텐츠,TOC 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Contents from "./components/Contents";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div>
         <Subject title="Web" sub="www"></Subject>
+        <TOC></TOC>
+        <Contents></Contents>
       </div>
     );
   }
---
+++ src/components/TOC.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class TOC extends Component {
+  render() {
+    return (
+      <article>
+        <ul>
+          <li>
+            <a href="/">HTML</a>
+          </li>
+          <li>
+            <a href="/">CSS</a>
+          </li>
+          <li>
+            <a href="/">JS</a>
+          </li>
+        </ul>
+      </article>
+    );
+  }
+}
+
+export default TOC;
